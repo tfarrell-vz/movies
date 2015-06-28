cec54ff36cd08f9d2a4466d265fb41e7abd8a8bf
tfarrell-vz <farrell@abiresearch.com>
2015-06-27 21:16:25 -0400

    Add The Matrix and Eternal Sunshine

--- entertainment_center.py
+++ entertainment_center.py
@@ -10,10 +10,22 @@ transformers = media.Movie("Transformers: The Movie",
                            "https://www.youtube.com/watch?v=4lo7JPLJUUU",
                            )
 
+the_matrix = media.Movie("The Matrix",
+                         "How would you know the difference between the dream world and the real world?",
+                         "http://www.impawards.com/1999/posters/matrix_ver1.jpg",
+                         "https://www.youtube.com/watch?v=m8e-FF8MsqU"
+                         )
+
+eternal_sunshine = media.Movie("Eternal Sunshine of the Spotless Mind",
+                               "Memories",
+                               "https://infinitecrescendo.files.wordpress.com/2014/03/"
+                               "eternal-sunshine-of-the-spotless-mind-poster.jpg",
+                               "https://www.youtube.com/watch?v=lnSgSe2GzDc"
+                               )
 
 
 # Package the movie instances into a List of movies.
-movies = [transformers]
+movies = [transformers, the_matrix, eternal_sunshine]
 
 # Generate the html and open the browser to it.
 fresh_tomatoes.open_movies_page(movies)
