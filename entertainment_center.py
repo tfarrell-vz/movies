ae76a4fe32a6525713cbdf7d3fb459ed5fdb7771
tfarrell-vz <farrell@abiresearch.com>
2015-06-28 21:49:41 -0400

    Added Star Wars

--- entertainment_center.py
+++ entertainment_center.py
@@ -28,8 +28,23 @@ lotr_two_towers = media.Movie("Lord of the Rings: The Two Towers",
                               "http://img4.wikia.nocookie.net/__cb20150203041214/lotr/images/9/98/The_two_tower.jpg",
                               "https://www.youtube.com/watch?v=LbfMDwc4azU")
 
+star_wars = media.Movie("Star Wars",
+                        "A long time ago in a galaxy far, far away...",
+                        "http://pre01.deviantart.net/6628/th/pre/i/2013/057/3/8/star_wars_iv___"
+                        "a_new_hope___movie_poster_by_nei1b-d5t3cw9.jpg",
+                        "https://www.youtube.com/watch?v=1g3_CFmnU7k"
+                        )
+
+empire_strikes_back = media.Movie("Star Wars: The Empire Strikes Back",
+                                  "After the destruction of the death star, the Empire seeks to annihilate the rebel"
+                                  "alliance. Darth Vader relentlessly pursues the young jedi, Luke Skywalker.",
+                                  "http://i.newsarama.com/images/i/000/138/527/original/"
+                                  "star-wars-empire-strikes-back-poster.jpg?1415977989",
+                                  "https://www.youtube.com/watch?v=mz_YWNhKOkM"
+                                  )
+
 # Package the movie instances into a List of movies.
-movies = [transformers, the_matrix, eternal_sunshine, lotr_two_towers]
+movies = [transformers, the_matrix, eternal_sunshine, lotr_two_towers, star_wars, empire_strikes_back]
 
 # Generate the html and open the browser to it.
 fresh_tomatoes.open_movies_page(movies)
