e0e1661813557ff9061ea07ae4a00e09464b9aaa
tfarrell-vz <farrell@abiresearch.com>
2015-06-29 23:09:22 -0400

    Added field for starring actors

--- media.py
+++ media.py
@@ -6,12 +6,13 @@ import webbrowser
 
 class Movie():
     def __init__(self, movie_title, movie_storyline, poster_image,
-                 trailer_youtube, duration):
+                 trailer_youtube, duration, stars):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
         self.duration = duration  # minutes
+        self.stars = stars  # starring actors and actresses
 
     def show_trailer(self):
         """
