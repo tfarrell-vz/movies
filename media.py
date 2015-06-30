210dc8e0a7fce4359a446cb9e6788056c318d24c
tfarrell-vz <farrell@abiresearch.com>
2015-06-29 22:42:17 -0400

    Comments; removed unused attribute

--- media.py
+++ media.py
@@ -6,12 +6,12 @@ import webbrowser
 
 class Movie():
     def __init__(self, movie_title, movie_storyline, poster_image,
-                 trailer_youtube, duration, cast):
+                 trailer_youtube, duration):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-        self.duration = duration
+        self.duration = duration  # minutes
 
     def show_trailer(self):
         """
