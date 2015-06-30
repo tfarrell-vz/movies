ff65c195037089674e8d053da53fa1f17672eb27
tfarrell-vz <farrell@abiresearch.com>
2015-06-29 22:30:51 -0400

    Added duration attribute

--- media.py
+++ media.py
@@ -3,12 +3,15 @@ import webbrowser
 
 # The movie class serves as a data container for movie information to be used
 # in movie tiles on the movie trailer website.
+
 class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube, duration, cast):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+        self.duration = duration
 
     def show_trailer(self):
         """
